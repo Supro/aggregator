0f2196c649c6a5641a7fe1f90a4e2ef5a6e0b641
Roman Budnikov <budnikov1990@gmail.com>
2015-10-30 02:27:15 +0300

    init

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,5 @@
+#= require jquery
+#= require react
+#= require react_ujs
+#= require components
+#= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aggregator::Application.config.secret_key_base = '409176a5cb9bb7dd11ffae5bb2e3529add3feac73d7afaa3a0469b553217f3c3381e9990cdac78e8988df405435870a60e58f571a1d7b5a0ca330db4dcb19947'
