5a6f9953bc4eb18be6e05b86b7b30442d5ca851f
Roman Budnikov <budnikov1990@gmail.com>
2016-04-10 14:39:15 +0300

    urls fix

--- app/serializers/api/v1/url_index_serializer.rb
+++ app/serializers/api/v1/url_index_serializer.rb
@@ -1,8 +1,12 @@
 class Api::V1::UrlIndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :context, :path, :image, :state, :pub_id
+  attributes :id, :title, :context, :path, :image, :state, :pub_id, :time
 
   def context
     object.context
+  end
+
+  def time
+    (object.created_at.to_f * 1000).to_i
   end
 
   def pub_id
