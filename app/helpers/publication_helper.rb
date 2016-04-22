3a5b27f677a11c6eb5f9da9368ede9d108e01d89
Roman Budnikov <budnikov1990@gmail.com>
2016-04-22 19:55:28 +0300

    sitemap change

--- app/helpers/publication_helper.rb
+++ app/helpers/publication_helper.rb
@@ -1,7 +1,11 @@
 module PublicationHelper
 
   def youtube_embed(youtube_url)
-    youtube_id = youtube_url.split("=").last
+    if youtube_url =~ /youtube.com/
+      youtube_id = youtube_url.split("=").last
+    else
+      youtube_id = youtube_url.split("/").last
+    end
     content_tag(:iframe, nil, { src: "//www.youtube.com/embed/#{youtube_id}", allowfullscreen: true, frameborder: "0", class: "youtube-video"})
   end
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,7 +8,18 @@ SitemapGenerator::Sitemap.create do
   add '/about', priority: 0.8
   add '/contacts', priority: 0.8
 
-  Publication.where(state: 'published').find_each do |pub|
-    add pub.type_path, lastmod: pub.updated_at, priority: 1
+  Publication.where(state: 'published', type: 'news').find_each do |pub|
+    add pub.type_path, lastmod: pub.published_at, priority: 1
+    #add pub.type_path, lastmod: pub.published_at, priority: 1, news: {
+    #    publication_name: "FireImp",
+    #    publication_language: "ru",
+    #    title: pub.title,
+    #    publication_date: pub.published_at.iso8601#,
+    #    #keywords: 'technology, video games'
+    #}
+  end
+
+  Publication.where(state: 'published').where.not(type: 'news').find_each do |pub|
+    add pub.type_path, lastmod: pub.published_at, priority: 1
   end
 end
